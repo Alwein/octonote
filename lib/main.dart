7eb8ef452b6153e4c017414cae78d6154db4c80d
Alwein <adrien.cens@edu.esiee.fr>
2022-05-02 14:29:50 +0200

    add firebase config for mobile/web/macOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:octonote/firebase_options.dart';
 import 'package:octonote/locator.dart' as sl;
 import 'package:octonote/presentation/app/app.dart';
 import 'package:octonote/presentation/app/bloc_observer.dart';
@@ -9,6 +11,9 @@ import 'package:octonote/presentation/app/bloc_observer.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sl.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
   await Hive.initFlutter();
   BlocOverrides.runZoned(
